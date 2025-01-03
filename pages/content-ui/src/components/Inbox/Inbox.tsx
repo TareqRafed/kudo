1ba5e2528801e5bd7a779c839fe31dad98c19170
TareqRafed <tareq.rafed22@gmail.com>
2025-01-03 23:22:02 +0300

    chore: removing placeholders

--- pages/content-ui/src/components/Inbox/Inbox.tsx
+++ pages/content-ui/src/components/Inbox/Inbox.tsx
@@ -1,5 +1,14 @@
+import useToolbarStore from '@src/store/toolbar';
+
 const Inbox = () => {
-  return <div className="z-max bg-background fixed right-10 top-10 h-[70%]">This is the inbox, can you see it?</div>;
+  const { toolbar } = useToolbarStore();
+
+  if (!toolbar.inbox.inUse) return <></>;
+  return (
+    <div className="z-max bg-background dark fixed right-10 top-10 h-[90%] rounded border p-2">
+      This is the inbox, can you see it?
+    </div>
+  );
 };
 
 export default Inbox;
